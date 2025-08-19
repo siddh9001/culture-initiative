94731074e9983f55c57f96039104b53e4f57af6c
Siddhu <siddhup0109@gmail.com>
2025-08-19 23:57:34 +0530

    fixed the insert functionality by modifying the query.

--- lib/neo4j/queries.ts
+++ lib/neo4j/queries.ts
@@ -10,5 +10,17 @@ export const queries = {
   `,
   getRecentlyAddedNodesQuery: () => `MATCH (p:Person) RETURN p.person_id AS id, p.person_name AS person_name, p.person_surname AS person_surname ORDER BY p.updated_at DESC LIMIT 5`,
   getFilterByIdQuery: (personKey: string) => `MATCH (p:Person) WHERE p.person_id = '${personKey}' RETURN p.person_id AS id, p.person_name AS person_name, p.person_surname AS person_surname`,
-  getInsertNewNodeQuery: (personObj: Object) => `CREATE (p:Person ${JSON.stringify(personObj)}) return p`,
+  getInsertNewNodeQuery: (personObj: any) => `CREATE (p:Person {
+      person_id: "${personObj.person_id}",
+      person_name: "${personObj.person_name}",
+      person_surname: "${personObj.person_surname}",
+      person_dob: "${personObj.person_dob}",
+      person_birth_place: "${personObj.person_birth_place}",
+      person_modified_name: "${personObj.person_modified_name}",
+      person_gender: "${personObj.person_gender}",
+      person_marrige_status: "${personObj.person_marrige_status}",
+      person_D_A_status: "${personObj.person_D_A_status}",
+      person_sasuraal: "${personObj.person_sasuraal}",
+      person_mayka: "${personObj.person_mayka}"
+    }) return p`,
 };
